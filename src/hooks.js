88147492aac2199e58df0bbffa0eaf2038853b73
Eric Moore <ericmoore0709@gmail.com>
2024-09-21 14:40:49 -0700

    useAxios Part 1

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import { v1 as uuid } from "uuid";
 
 const useFlip = () => {
     const [isFacingUp, setIsFacingUp] = useState(true);
@@ -9,4 +11,13 @@ const useFlip = () => {
     return [isFacingUp, flipCard];
 }
 
-export { useFlip }
+const useAxios = (url) => {
+    const [data, setData] = useState([]);
+    const addData = async () => {
+        const response = await axios.get(url);
+        setData(data => [...data, { ...response.data, id: uuid() }]);
+    };
+    return [data, addData];
+}
+
+export { useFlip, useAxios }
